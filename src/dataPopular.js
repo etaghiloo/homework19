0d72ba78ad2dc14c1eef0671aac5ddf614d2d718
echolima <elnaz.taghiloo@gmail.com>
2023-05-20 10:10:24 +0430

    first commit

---
+++ src/dataPopular.js
@@ -0,0 +1,96 @@
+export const popular01 = [
+    {
+        image: "assets/images/mov_134077_56979.jpg",
+        link: "https://www.filimo.com/m/132691",
+        isExclusive: false,
+    },
+    {
+        image: "assets/images/mov_133828_56868.jpg",
+        link: "https://www.filimo.com/m/133827",
+        isExclusive: true,
+    },
+    {
+        image: "assets/images/mov_134076_56936.jpg",
+        link: "https://www.filimo.com/m/131941",
+        isExclusive: false,
+    },
+    {
+        image: "assets/images/mov_134075_56938.jpg",
+        link: "https://www.filimo.com/m/132689",
+        isExclusive: true,
+    },
+    {
+        image: "assets/images/mov_133845_56819.jpg",
+        link: "https://www.filimo.com/m/131383",
+        isExclusive: true,
+    },
+    {
+        image: "assets/images/mov_133843_56806.jpg",
+        link: "https://www.filimo.com/m/130789",
+        isExclusive: false,
+    },
+]
+export const popular02 = [
+    {
+        image: "assets/images/mov_132148_54299-s.webp",
+        link: "https://www.filimo.com/m/132691",
+        isExclusive: true,
+    },
+    {
+        image: "assets/images/mov_131147_53880-s.webp",
+        link: "https://www.filimo.com/m/133827",
+        isExclusive: true,
+    },
+    {
+        image: "assets/images/mov_131938_54213-s.webp",
+        link: "https://www.filimo.com/m/131941",
+        isExclusive: false,
+    },
+    {
+        image: "assets/images/mov_125076_56170-s.webp",
+        link: "https://www.filimo.com/m/132689",
+        isExclusive: true,
+    },
+    {
+        image: "assets/images/mov_132897_55350-s.webp",
+        link: "https://www.filimo.com/m/131383",
+        isExclusive: false,
+    },
+    {
+        image: "assets/images/mov_130962_52696-s.webp",
+        link: "https://www.filimo.com/m/130789",
+        isExclusive: true,
+    },
+]
+export const popular03 = [
+    {
+        image: "assets/images/mov_639_995-s.webp",
+        link: "https://www.filimo.com/m/132691",
+        isExclusive: true,
+    },
+    {
+        image: "assets/images/mov_9258_27939-s.webp",
+        link: "https://www.filimo.com/m/133827",
+        isExclusive: false,
+    },
+    {
+        image: "assets/images/mov_11381_315-s.webp",
+        link: "https://www.filimo.com/m/131941",
+        isExclusive: true,
+    },
+    {
+        image: "assets/images/mov_15804_16979-s.webp",
+        link: "https://www.filimo.com/m/132689",
+        isExclusive: false,
+    },
+    {
+        image: "assets/images/mov_24190_1-s.webp",
+        link: "https://www.filimo.com/m/131383",
+        isExclusive: false,
+    },
+    {
+        image: "assets/images/mov_30040_808-s.webp",
+        link: "https://www.filimo.com/m/130789",
+        isExclusive: false,
+    },
+]
--- src/index.js
+++ src/index.js
@@ -3,12 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MovieList from './Components/movieList';
+import FilimoBox from './Components/FilimoBox';
+import {popular01} from './dataPopular';
+import {popular02} from './dataPopular';
+import {popular03} from './dataPopular';
+import {filimoBox01} from './dataFilimoBox';
+import {filimoBox02} from './dataFilimoBox';
+import {filimoBox03} from './dataFilimoBox';
+import './styleGlobal.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <MovieList data={popular01} title="محبوب‌ترین‌های فیلیمو .یک" />
+    <MovieList data={popular02} title="محبوب‌ترین‌های فیلیمو .دو" />
+    <MovieList data={popular03} title="محبوب‌ترین‌های فیلیمو .سه" />
+    <FilimoBox data={filimoBox01} title="فیلیمو باکس چیست؟" description="برای تماشای فیلیمو، تلویزیون شما باید هوشمند باشد، اگر نیست، با تهیه یک اندروید باکس، تلویزیون خود را هوشمند کنید، ما به شما فیلیمو باکس را پیشنهاد می‌کنیم که به تلویزیون وصل می‌شود و آن را هوشمند می‌کند. چطوری؟ کافیست ویدیو‌های زیر را تماشا‌کنید." />
+    <FilimoBox data={filimoBox02} title="تلویزیون ما سیستم عامل اندروید دارد." description="اگر روی تلویزیون شما امکان نصب‌ برنامه وجود دارد، کافیست، برنامه Filimo را از گوگل‌پلی جستجو و روی تلویزیون خود نصب کنید. روش نصب برنامه فیلیمو را در ویدیوی مربوط به آن کامل توضیح دادیم." />
+    <FilimoBox data={filimoBox03} title="دیدن صفحه گوشی روی تلویزیون(کست‌کردن با گوشی)" description="اگر بخواهید فیلمی که در صفحه گوشی می‌بینید روی صفحه تلویزیون هم ببینید،کافیست، علامت کست را فقط در سایت نه در اپلیکیشن، موقع پخش فیلم، گوشه تصویر پیدا کنید و روی آن بزنید، برای جزییات کامل‌تر ویدیوی“کست کردن” را تماشا کنید." />
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
